280446a56f055db224a5770b01bb243a2b046b2f
RomanianKiwi <ciubotariu.flavien@gmail.com>
2015-03-01 11:16:06 +0100

    creation de l'arborescence du projet et importation des dépendances

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Questionnaire SUS</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $("#here").text("Hello World !");
+            });
+        </script>
+    </head>
+    <body>
+		<p id="here"></p>
+        <?php
+        // code here
+        ?>
+    </body>
+</html>
